83cd137b1d303e22c35637fc291ad811c050e861
truongthanhdat <1512107@student.hcmus.edu.vn>
2018-03-02 11:39:07 +0700

    Data Preparing

---
+++ data.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+corpus_raw = 'He is the king . The king is royal . She is the royal  queen '
+
+# convert to lower case
+corpus_raw = corpus_raw.lower()
+
+words = []
+for word in corpus_raw.split():
+    if word != '.': # because we don't want to treat . as a word
+        words.append(word)
+
+words = set(words) # so that all duplicate words are removed
+word2int = {}
+int2word = {}
+vocab_size = len(words) # gives the total number of unique words
+
+for i,word in enumerate(words):
+    word2int[word] = i
+    int2word[i] = word
+
+# raw sentences is a list of sentences.
+raw_sentences = corpus_raw.split('.')
+sentences = []
+for sentence in raw_sentences:
+    sentences.append(sentence.split())
+
+WINDOW_SIZE = 2
+
+data = []
+for sentence in sentences:
+    for word_index, word in enumerate(sentence):
+        for nb_word in sentence[max(word_index - WINDOW_SIZE, 0) : min(word_index + WINDOW_SIZE, len(sentence)) + 1] :
+            if nb_word != word:
+                data.append([word, nb_word])
+
+# function to convert numbers to one hot vectors
+def to_one_hot(data_point_index, vocab_size):
+    temp = np.zeros(vocab_size)
+    temp[data_point_index] = 1
+    return temp
+
+x_train = [] # input word
+y_train = [] # output word
+
+for data_word in data:
+    x_train.append(to_one_hot(word2int[ data_word[0] ], vocab_size))
+    y_train.append(to_one_hot(word2int[ data_word[1] ], vocab_size))
+
+# convert them to numpy arrays
+x_train = np.asarray(x_train)
+y_train = np.asarray(y_train)
