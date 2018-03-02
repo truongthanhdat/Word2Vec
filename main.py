fee3b995aa4824ac4d764fdfb730aad9b9eb9b9a
truongthanhdat <1512107@student.hcmus.edu.vn>
2018-03-02 14:09:08 +0700

    Traning

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import model
 from data import x_train, y_train, vocab_size
 import tensorflow as tf
+import argparse
 
-EMB = 5
+parser = argparse.ArgumentParser()
+parser.add_argument('--size', type=int, help='Embedding size', default=16)
+parser.add_argument('--lr', type=float, help='Learning Rate', default=0.1)
+parser.add_argument('--iters', type=int, help='Number of interations', default=100)
+option = parser.parse_args()
+
+EMB = option.size
 VOC = vocab_size
 
 w2v = model.Word2Vec(VOC, EMB, True, x_train, y_train)
 sess = tf.Session()
 
-w2v.train(100000, sess)
+w2v.train(option.iters, option.lr, sess)
 
