8801348a0dc4906a4893f15e796fe6094a93ba03
truongthanhdat <1512107@student.hcmus.edu.vn>
2018-03-02 14:08:56 +0700

    Demo

--- demo.py
+++ demo.py
@@ -3,11 +3,19 @@ import model
 import numpy as np
 import tensorflow as tf
 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--size', type=int, help='Embedding size', default=16)
+parser.add_argument('-k', type=int, help='Number of retrieval', default=10)
+parser.add_argument('--input', type=str, help='Word', default='he')
+option = parser.parse_args()
+
 weights = np.load('weights.npy').item()
 
-w2v = model.Word2Vec(data.vocab_size, 5, _isTrain=False, _weights=weights)
+w2v = model.Word2Vec(data.vocab_size, option.size, _isTrain=False, _weights=weights)
 
-voca = 'he'
+voca = option.input.lower()
 
 vec = np.zeros((1, data.vocab_size))
 vec[0][data.word2int[voca]] = 1
@@ -19,6 +27,6 @@ predict = sess.run(w2v.predict, feed_dict={w2v.input: vec})
 
 rank = np.argsort(predict[0])
 
-for i in rank:
+for i in rank[:option.k]:
     print data.int2word[i]
 
