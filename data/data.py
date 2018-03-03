537014cdece4c751339bf7c1b9777eecc577122d
truongthanhdat <1512107@student.hcmus.edu.vn>
2018-03-04 00:01:35 +0700

    Data Processing

---
+++ data/data.py
@@ -0,0 +1,95 @@
+import zipfile
+import numpy as np
+import tensorflow as tf
+import collections
+import random
+
+def read_data(filename):
+  """Extract the first file enclosed in a zip file as a list of words."""
+  with zipfile.ZipFile(filename) as f:
+    data = tf.compat.as_str(f.read(f.namelist()[0])).split()
+  return data
+
+filename = 'data/text8.zip'
+vocabulary = read_data(filename)
+print('Data size', len(vocabulary))
+
+# Step 2: Build the dictionary and replace rare words with UNK token.
+vocabulary_size = 50000
+
+
+def build_dataset(words, n_words):
+  """Process raw inputs into a dataset."""
+  count = [['UNK', -1]]
+  count.extend(collections.Counter(words).most_common(n_words - 1))
+  dictionary = dict()
+  for word, _ in count:
+    dictionary[word] = len(dictionary)
+  data = list()
+  unk_count = 0
+  for word in words:
+    index = dictionary.get(word, 0)
+    if index == 0:  # dictionary['UNK']
+      unk_count += 1
+    data.append(index)
+  count[0][1] = unk_count
+  reversed_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+  return data, count, dictionary, reversed_dictionary
+
+
+# Filling 4 global variables:
+# data - list of codes (integers from 0 to vocabulary_size-1).
+#   This is the original text but words are replaced by their codes
+# count - map of words(strings) to count of occurrences
+# dictionary - map of words(strings) to their codes(integers)
+# reverse_dictionary - maps codes(integers) to words(strings)
+data, count, dictionary, reverse_dictionary = build_dataset(
+    vocabulary, vocabulary_size)
+del vocabulary  # Hint to reduce memory.
+#print('Most common words (+UNK)', count[:5])
+#print('Sample data', data[:10], [reverse_dictionary[i] for i in data[:10]])
+
+data_index = 0
+
+
+# Step 3: Function to generate a training batch for the skip-gram model.
+def generate_batch(batch_size, num_skips, skip_window):
+  global data_index
+  assert batch_size % num_skips == 0
+  assert num_skips <= 2 * skip_window
+  batch = np.ndarray(shape=(batch_size), dtype=np.int32)
+  labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
+  span = 2 * skip_window + 1  # [ skip_window target skip_window ]
+  buffer = collections.deque(maxlen=span)
+  if data_index + span > len(data):
+    data_index = 0
+  buffer.extend(data[data_index:data_index + span])
+  data_index += span
+  for i in range(batch_size // num_skips):
+    context_words = [w for w in range(span) if w != skip_window]
+    words_to_use = random.sample(context_words, num_skips)
+    for j, context_word in enumerate(words_to_use):
+      batch[i * num_skips + j] = buffer[skip_window]
+      labels[i * num_skips + j, 0] = buffer[context_word]
+    if data_index == len(data):
+      buffer.extend(data[0:span])
+      data_index = span
+    else:
+      buffer.append(data[data_index])
+      data_index += 1
+  # Backtrack a little bit to avoid skipping words in the end of a batch
+  data_index = (data_index + len(data) - span) % len(data)
+  M = len(batch)
+  N = vocabulary_size
+  x = np.zeros((M, N), dtype=np.float32)
+  y = np.zeros((M, N), dtype=np.float32)
+  for i in xrange(M):
+    x[i][batch[i]] = 1
+    y[i][labels[i]] = 1
+  #return batch, labels
+  return x, y
+
+
+#batch, labels = generate_batch(batch_size=8, num_skips=2, skip_window=1)
+#print batch
+#print labels
